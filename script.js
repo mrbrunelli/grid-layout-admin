da0c4f0eda7138164941a11392710a94acef667d
Matheus Ricardo Brunelli <matheus.brunelli@gmail.com>
2020-07-20 20:05:09 -0300

    add switch toggle for sidebar

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-const sidebar = document.querySelector('#sidebar')
-
+const sidebar = document.querySelector('#container')
+/** grid-template-columns: minmax(100px, 200px) auto */
+let state = 1
 function toggle() {
-    sidebar.style.display = 'none'
+    if (state === 1) {
+        sidebar.style.gridTemplateColumns = 'minmax(100px, 100px) auto'
+        state = 0
+    } else {
+        sidebar.style.gridTemplateColumns = 'minmax(100px, 250px) auto'
+        state = 1
+    }
 }
